8e65404b822d19af14d6ccb4613b11857c31373d
C_code <564309357@qq.com>
2019-11-19 18:16:12 +0800

    堆排序

---
+++ 8_16/HeapSort.h
@@ -0,0 +1,58 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<assert.h>
+void Swap(int* p1, int* p2)
+{
+	int tmp = *p1;
+	*p1 = *p2;
+	*p2 = tmp;
+}
+PrintArray(int* a, int n)
+{
+	for (int i = 0; i < n; i++)
+	{
+		printf("%d ", a[i]);
+	}
+}
+void AdjustDown(int* a, int n, int root)
+{
+	int parent = root;
+	int child = parent * 2 + 1;
+	while (child < n)
+	{
+		if (child + 1 < n&&a[child + 1] > a[child])
+		{
+			++child;
+		}
+		if (a[child]>a[parent])
+		{
+			Swap(&a[child], &a[parent]);
+			parent = child;
+			child = parent * 2 + 1;
+		}
+		else
+		{
+			break;
+		}
+	}
+}
+void HeapSort(int* a, int n)
+{
+	for (int i = (n - 2) / 2; i >= 0; --i)
+	{
+		AdjustDown(a, n, i);
+	}
+	int end = n - 1;
+	while (end > 0)
+	{
+		Swap(&a[0], &a[end]);
+		AdjustDown(a, end, 0);
+		--end;
+	}
+}
+void HeapSortTest()
+{
+	int arr[] = {3,4,7,8,2,6,1,5,9};
+	HeapSort(arr, sizeof(arr) / sizeof(int));
+	PrintArray(arr, sizeof(arr) / sizeof(int));
+}
---
+++ 8_16/test.c
@@ -0,0 +1,7 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include"HeapSort.h"
+int main()
+{
+	HeapSortTest();
+	return 0;
+}
