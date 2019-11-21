b5458d0816d0084811f1ac016013cfbf4a32c8bc
NewLearner55 <53102990+NewLearner55@users.noreply.github.com>
2019-11-21 18:46:44 +0800

    Update test.c

--- 8_20/test.c
+++ 8_20/test.c
@@ -1,7 +1,42 @@
 #define _CRT_SECURE_NO_WARNINGS 1
-#include"QuickSort.h"
-int main()
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     struct TreeNode *left;
+ *     struct TreeNode *right;
+ * };
+ */
+/**
+ * Return an array of size *returnSize.
+ * Note: The returned array must be malloced, assume caller calls free().
+ */
+int GetTreeSize(struct TreeNode* root)
 {
-	QuickSort();
-	return 0;
+    if(root == NULL) 
+        return 0;
+    else
+        return GetTreeSize(root->left) + GetTreeSize(root->right) + 1;
+}
+
+void _inorderTraversal(struct TreeNode* root, int* array, int* pIndex)
+{
+    if(root == NULL)
+        return;
+    
+    _inorderTraversal(root->left, array, pIndex);
+    
+    array[*pIndex] = root->val;
+    (*pIndex)++;
+    
+   _inorderTraversal(root->right, array, pIndex); 
+}
+
+int* inorderTraversal(struct TreeNode* root, int* returnSize) {
+    *returnSize = GetTreeSize(root);
+    int* array = (int*)malloc(*returnSize*sizeof(int));
+    int index = 0;
+    _inorderTraversal(root, array, &index);
+    
+    return array;
 }
